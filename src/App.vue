11d0fad82cb2dae59038d13acb4426970938fd6d
Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>
2017-10-13 20:23:50 -0400

    initial commit, scaffolding

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <router-view></router-view>
+  <v-app>
+
+  <v-toolbar>
+    <v-toolbar-title>Student Record</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Current PhD</v-btn>
+      <v-btn flat>MS(PhD Track)</v-btn>
+      <v-btn flat>Current Funded MS Only</v-btn>
+      <v-btn flat>Graduated PhD</v-btn>
+      <v-btn flat>Graduated MS</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+
+  </v-app>
+</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import '../stylus/main'
+</style>
