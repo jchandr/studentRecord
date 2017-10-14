1a0b1c2f31476d0315c92812a7f6d8519dc6d584
Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>
2017-10-13 22:06:55 -0400

    buttons homescreen

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,67 @@
 <template>
-  <div>
+    <v-app>
     <router-view></router-view>
-  <v-app>
-
   <v-toolbar>
     <v-toolbar-title>Student Record</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Current PhD</v-btn>
-      <v-btn flat>MS(PhD Track)</v-btn>
-      <v-btn flat>Current Funded MS Only</v-btn>
-      <v-btn flat>Graduated PhD</v-btn>
-      <v-btn flat>Graduated MS</v-btn>
+      <v-btn v-for="tool of toolBarItems"
+      :key="tool.title"
+      @click="handleButtonClick"
+      flat>{{ tool.title }}</v-btn>
+
     </v-toolbar-items>
   </v-toolbar>
-
   </v-app>
-</div>
+
 </template>
 
 <script>
   export default {
     data () {
       return {
+        toolBarItems: [
+          {
+            title: 'Current Phd',
+            isSelected: {
+              type: Boolean,
+              default: true
+            }
+          },
+          {
+            title: 'MS (Phd Track)',
+            isSelected: {
+              type: Boolean,
+              default: false
+            }
+          },
+          {
+            title: 'Current Funded MS Only',
+            isSelected: {
+              type: Boolean,
+              default: false
+            }
+          },
+          {
+            title: 'Graduated Phd',
+            isSelected: {
+              type: Boolean,
+              default: false
+            }
+          },
+          {
+            title: 'Graduated MS',
+            isSelected: {
+              type: Boolean,
+              default: false
+            }
+          }
+        ]
+      }
+    },
+    methods: {
+      handleButtonClick () {
       }
     }
   }
