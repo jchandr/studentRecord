68b9139e606bfa986507a08c16b4cd856db835be
Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>
2017-10-19 02:35:02 -0400

    login page text fields added, needs styling

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,45 @@
 <template>
   <div>
-    <v-btn @click="handleClick">Hello</v-btn>
+    <v-form v-model="valid"
+    class="login-form"
+    ref="form"
+    lazy-validation>
+      <v-text-field
+        label="E-mail"
+        v-model="email"
+        :rules="emailRules"
+        required></v-text-field>
+        <v-flex>
+              <v-text-field
+                name="input-10-1"
+                label="Enter your password"
+                hint="At least 8 characters"
+                v-model="password"
+                min="8"
+                :append-icon="e1 ? 'visibility_off' : 'visibility'"
+                :append-icon-cb="() => (e1 = !e1)"
+                :type="e1 ? 'text' : 'password'"
+                counter></v-text-field>
+        </v-flex>
+      <v-btn @click="submit"
+      :disabled="!valid">submit</v-btn>
+      <v-btn @click="clear">clear</v-btn>
+    </v-form>
   </div>
 </template>
 
 <script>
 import RouteNames from '@/router/names'
+
 export default {
   name: 'LoginPage',
+  data () {
+    return {
+      email: undefined,
+      e1: false,
+      password: undefined
+    }
+  },
   methods: {
     handleClick () {
       this.$router.push({
@@ -20,4 +52,9 @@ export default {
 
 <style lang="stylus">
   @import '../stylus/main'
+
+  .login-form
+    align: center;
+    width: 20em;
+
 </style>
