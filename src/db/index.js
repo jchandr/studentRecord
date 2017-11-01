0e613c8953c7af78b90aa5662babc6b098d48e48
Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>
2017-11-01 02:03:04 -0400

    aws fix

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,5 @@
-// const API_URL = 'http://52.38.180.245:3001/'
-const API_URL = 'http://localhost:3001/'
+const API_URL = 'http://52.38.180.245:3001/'
+  // const API_URL = 'http://localhost:3001/'
 
 export default {
   getMSStudentRecords (context) {
